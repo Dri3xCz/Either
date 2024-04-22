038fc411b8debaab8bc2ce002694cbd845ce9742
Lukáš Hak <hakluka@student.uzlabina.cz>
2024-04-22 15:00:06 +0200

    init

---
+++ Either.cs
@@ -0,0 +1,39 @@
+﻿namespace Either
+{
+    public abstract class Either<E, T>
+    {
+        // LEFT => ERROR HANDLING
+        // RIGHT => ACTUAL VALUE
+        public abstract void Fold(Action<E> leftMethod, Action<T> rightMethod);
+    }
+
+    public class Left<E, T> : Either<E, T>
+    {
+        E ex;
+
+        public Left(E ex)
+        {
+            this.ex = ex;
+        }
+
+        public override void Fold(Action<E> leftMethod, Action<T> rightMethod)
+        {
+            leftMethod(ex);
+        }
+    }
+
+    public class Right<E, T> : Either<E, T>
+    {
+        T value;
+
+        public Right(T value)
+        {
+            this.value = value;
+        }
+
+        public override void Fold(Action<E> leftMethod, Action<T> rightMethod)
+        {
+            rightMethod(value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Either;
+
+class Program
+{
+    static void Main()
+    {
+        Either<Exception, float> myNum1 = DivideNumber(10, 0);
+
+        myNum1.Fold(
+            (ex) => {
+                Console.WriteLine(ex.Message);
+            },
+            (value) =>
+            {
+                Console.WriteLine(value.ToString());
+            }
+        );
+
+        Either<Exception, float> myNum2 = DivideNumber(10, 5);
+
+        myNum2.Fold(
+            (ex) => {
+                Console.WriteLine(ex.Message);
+            },
+            (value) =>
+            {
+                Console.WriteLine(value.ToString());
+            }
+        );
+    }
+
+    static Either<Exception, float> DivideNumber(float number, float divider)
+    {
+        if (divider == 0)
+        {
+            return new Left<Exception, float>(new DivideByZeroException());
+        }
+
+        float result = number / divider;
+
+        return new Right<Exception, float>(result);
+    }
+}
